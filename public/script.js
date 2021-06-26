d48f088c1f04e6b52850430ee2c1bae099c76942
shanukrsingh <shanukrsinghhis@gmail.com>
2021-06-27 01:45:00 +0530

    version 2.0.2

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure: true,
-    host: 'letsvideomeet.herokuapp.com',
+    host: "letsvideomeet.herokuapp.com",
     port: 9000
 })
 

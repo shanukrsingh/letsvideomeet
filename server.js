5b316ea251d009ff159ed2bbdacab210dc2b60b4
shanukrsingh <shanukrsinghhis@gmail.com>
2021-06-27 01:06:37 +0530

    version 1.0.7

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4} = require('uuid')
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 443, path: '/' });
+const peerServer = PeerServer({ port: 9000, path: '/' });
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))

f4913ba71e3eb78574af114f846528407d129981
shanukrsingh <shanukrsinghhis@gmail.com>
2021-06-27 01:36:56 +0530

    version 2.0.0

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
+    secure: true,
     host: 'letsvideomeet.herokuapp.com',
-    port: '9000'
+    port: 443
 })
 
 const myVideo = document.createElement('video')
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4} = require('uuid')
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 9000, path: '/' });
+const peerServer = PeerServer({ port: 443, path: '/' });
 app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
